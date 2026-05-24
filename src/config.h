3ce080aa13b3df1505b47eca4647944f093a07c1
AchLim <rimuakoru@gmail.com>
2026-05-25 02:38:48 +0700

    [CMake] Properly remove raudio.c and rmodels.c if they're disabled modules

--- src/config.h
+++ src/config.h
@@ -45,12 +45,12 @@
 #ifndef SUPPORT_MODULE_RTEXT
     #define SUPPORT_MODULE_RTEXT        1       // WARNING: It requires SUPPORT_MODULE_RTEXTURES to load sprite font textures
 #endif
-#ifndef SUPPORT_MODULE_RMODELS
-    #define SUPPORT_MODULE_RMODELS      1
-#endif
-#ifndef SUPPORT_MODULE_RAUDIO
-    #define SUPPORT_MODULE_RAUDIO       1
-#endif
+// #ifndef SUPPORT_MODULE_RMODELS
+//     #define SUPPORT_MODULE_RMODELS      1
+// #endif
+// #ifndef SUPPORT_MODULE_RAUDIO
+//     #define SUPPORT_MODULE_RAUDIO       1
+// #endif
 
 //------------------------------------------------------------------------------------
 // Module: rcore - Configuration Flags
